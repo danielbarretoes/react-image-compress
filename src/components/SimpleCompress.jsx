027b9f18d59f5e16c88ea8eec94684a4dd59d27f
danielbarretoes <125984531+danielbarretoes@users.noreply.github.com>
2023-04-09 01:40:32 +0200

    API

--- src/components/SimpleCompress.jsx
+++ src/components/SimpleCompress.jsx
@@ -1,49 +1,25 @@
 import { useState } from "react";
-import imageCompression from "browser-image-compression";
+import { compressImageAttachment, downloadLocalAttachmentURL } from "./compressAPI";
 
 const SimpleCompress = () => {
   const [compressedFileURL, setCompressedFileURL] = useState("");
+
+  // Compress funtion
   const handleUpload = async (event) => {
-    const imageFile = event.target.files[0];
-    console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
-
-    const options = {
-      maxSizeMB: 1,
-      maxWidthOrHeight: 1920,
-      useWebWorker: true,
-    };
-
-    if (options.maxSizeMB >= imageFile / 1024) {
-      // smaller than maxSizeMB
-      alert("Image is too small, cant be compressed");
-      return 0;
-    }
-
-    try {
-      const compressedFile = await imageCompression(imageFile, options);
-
-      console.log(
-        "compressedFile instanceof Blob",
-        compressedFile instanceof Blob
-      ); // true
-
-      //console.log(URL.createObjectURL(compressedFile));
-      setCompressedFileURL(URL.createObjectURL(compressedFile));
-      return compressedFile;
-    } catch (error) {
-      console.log(error);
-    }
+    const imageAttachment = event.target.files[0];
+    const compressResult = await compressImageAttachment(imageAttachment);
+    setCompressedFileURL(downloadLocalAttachmentURL(compressResult));
   };
 
   return (
     <div>
-      <h2>SimpleCompress</h2>{" "}
+      <h2>SimpleCompress</h2>
       <input
         type="file"
         accept="image/*"
         onChange={(event) => handleUpload(event)}
       ></input>
-      <img src={compressedFileURL}></img>
+      <img src={compressedFileURL} alt="img"></img>
     </div>
   );
 };
